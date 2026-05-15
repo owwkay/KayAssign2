792dc6393a361c6a49a8d660da84e3761f2a71eb
owwkay <s4171044@student.rmit.edu.au>
2026-05-15 21:23:54 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-const video = document.getElementById("videoplay");
-const buttons = document.querySelectorAll(".timestamp-btn");
+const video = document.getElementById("videoplay"); /* getElementById = to find the video in my HTML */
+const buttons = document.querySelectorAll(".timestamp-btn"); /* Finds all buttons with class ".timestamp.btn*/
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const time = Number(button.dataset.time);
-    video.currentTime = time;
+buttons.forEach(button => { /* forEach = the code is repeated for each variable (button?) that I have*/
+  button.addEventListener("click", () => { /* Awaiting user interaction*/
+    const time = Number(button.dataset.time); /*The button's "data-time" value is turned into a number*/
+    video.currentTime = time; /* moves the video to the selected timestamp */
     video.play();
   });
 
