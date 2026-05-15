23cac20d3611439181cd03942702c0840ba4030b
owwkay <s4171044@student.rmit.edu.au>
2026-05-15 17:24:51 +1000

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const video = document.getElementById("videoplay");
+const buttons = document.querySelectorAll(".timestamp-btn");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const time = Number(button.dataset.time);
+    video.currentTime = time;
+    video.play();
+  });
+
+});
